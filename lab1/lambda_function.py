e454f2aaf7a0d7e06409108b698f00868c57b640
CloudPott <77894767+cloudpott@users.noreply.github.com>
2021-03-06 13:13:47 -0500

    Update lambda_function.py

--- lab1/lambda_function.py
+++ lab1/lambda_function.py
@@ -10,14 +10,13 @@ def lambda_handler(event, context):
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = unquote_plus(event['Records'][0]['s3']['object']['key'])
     response = rekognition.detect_moderation_labels(Image={'S3Object': {'Bucket': bucket, 'Name': key}})
-    try:
-        table = dynamodb.Table('contenido_imagenes')
-        time = int(datetime.datetime.now().timestamp())
-        response_dynamo = table.put_item(
-           Item={
-                'id': time,
-                'res':json.dumps(response['ModerationLabels'])
-            }
-        )
-    except Exception, e:
-        raise e
+
+    table = dynamodb.Table('image_content')
+    time = int(datetime.datetime.now().timestamp())
+    response_dynamo = table.put_item(
+       Item={
+            'id': time,
+            'res':json.dumps(response['ModerationLabels'])
+        }
+    )
+    
